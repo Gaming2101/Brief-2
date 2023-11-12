fe0aa9bf0bbc39f208e2a21791f1d986ccf64537
Gaming2101 <55343517+Gaming2101@users.noreply.github.com>
2023-11-12 20:46:07 +0800

    Add files via upload

---
+++ EnemyController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public static EnemyController me;
+    // assign
+    public Enemy[] enemies;
+
+    // ignore
+    public int currentEnemy;
+
+    private void Awake()
+    {
+        me = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // sets up the first enemy
+        enemies[currentEnemy].Ready();
+    }
+
+    // Switches to next enemy
+    public void NextEnemy()
+    {
+        if(currentEnemy == enemies.Length - 1)
+        {
+           // idiot put something here please for winning
+        }
+        else
+        {
+            currentEnemy += 1;
+            enemies[currentEnemy].Ready();
+        }
+    }
+}
